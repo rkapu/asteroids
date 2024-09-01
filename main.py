65e64e888129ed53664ec4cd6baa5782d2e5957c
rkapu <romankap1798@gmail.com>
2024-09-01 23:48:16 +0300

    Add player movement, use sprite groups

--- main.py
+++ main.py
@@ -12,17 +12,23 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-        player.update(dt)
-
         screen.fill("black")
-        player.draw(screen)
+
+        for u in updatable:
+            u.update(dt)
+        for d in drawable:
+            d.draw(screen)
+
         pygame.display.flip()
 
         # limit framerate to 60 fps
