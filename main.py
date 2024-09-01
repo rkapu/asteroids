f797f7bcc20545909f0e6022df449706ac964d9d
rkapu <romankap1798@gmail.com>
2024-09-01 23:28:46 +0300

    Add circleshape, draw player on screen

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from constants import *
 import pygame
 
+from player import Player
+
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -8,6 +10,9 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -15,7 +20,9 @@ def main():
                 return
 
         screen.fill(color="#000000")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
